53f13edab86655442952fb34a7501f38bb4308c9
Mallikarjun <mallikarjunkamtam@gmail.com>
2022-02-19 13:06:29 +0530

    Changed Footer and Header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
 function Footer() {
-  return <p className="foot">©️ Mallikarjun Kamtam</p>;
+  return <p className="foot">©️ Mallikarjun</p>;
 }
 export default Footer;
